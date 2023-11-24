ee50b1ebe805a2da6e83fc243a3cb9b93e8e23b9
Murimi Antony <amurimi101@gmail.com>
2023-11-24 10:40:01 +0300

    done

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,30 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fib = []  
+    
+    if length <= 0:
+        print([])
+    elif length == 1:
+        fib = [0]
+        print(fib)
+    elif length == 2:
+        fib = [0, 1]
+        print(fib)
+    else:
+        fib = [0, 1]
+        for _ in range(2, length):
+            next_fib = fib[-1] + fib[-2]
+            fib.append(next_fib)
+        print(fib)
+
+
+
+
+    
+    
+
+ 
+
+
+
